d19b887652b7ae42b6ac08a741770063523a0f0a
Jose R Seijas <seijasdz@gmail.com>
2026-01-09 13:27:24 -0500

    added gh actions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (_req, res) => {
-    res.send("Hello from elastic beanstalk!");
+    res.send("Hello from elastic beanstalk! After GH Act");
 })
 
 const PORT = process.env.PORT || 3000;
